ea3b7cac974abde48dd4c818c855b365c405497c
Antonio Rios <antonio.riosm@gmail.com>
2017-09-02 13:51:46 -0500

    Usando Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename');
+
+gulp.task('styles', () => {
+  gulp
+    .src('index.scss')
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('assets', () => {
+  gulp
+    .src('assets/*')
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['styles', 'assets']);
